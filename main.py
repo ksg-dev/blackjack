313024af2180f70cc672636cfc77b6b272966496
ksg-dev <ksg.dev.data@gmail.com>
2024-05-09 11:44:57 -0500

    loops and draws complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from clear import clear
 
 ############### Our Blackjack House Rules #####################
 
@@ -33,9 +34,16 @@ def calculate_score(hand):
         elif len(hand) == 2 and 10 in hand:
             return 0
         else:
-            return 11
+            return int(score)
     else:
         return int(score)
+
+
+# Final score print statement to save some repetition
+def final_score(user_hand, dealer_hand, user_score, dealer_score):
+    print(f"Your final hand: {user_hand}, final score: {user_score}")
+    print(f"Dealer's final hand: {dealer_hand}, final score: {dealer_score}")
+
 
 def main():
     playing = True
@@ -47,36 +55,37 @@ def main():
         # check for dealer blackjack
         if dealer_score == 0:
             playing = False
-            print(f"Your final hand: {user_hand}, final score: {user_score}")
-            print(f"Dealer's final hand: {dealer_hand}, final score: {dealer_score}")
+            final_score(user_hand, dealer_hand, user_score, dealer_score)
             print("The Dealer got BlackJack! You Lose!")
-        # Check scores
+        # Check for user blackjack
         elif user_score == 0:
             playing = False
-            print(f"Your final hand: {user_hand}, final score: {user_score}")
-            print(f"Dealer's final hand: {dealer_hand}, final score: {dealer_score}")
+            final_score(user_hand, dealer_hand, user_score, dealer_score)
             print("You got BlackJack! You win!")
+        # Check for bust
         elif user_score > 21:
             playing = False
-            print(f"Your final hand: {user_hand}, final score: {user_score}")
-            print(f"Dealer's final hand: {dealer_hand}, final score: {dealer_score}")
+            final_score(user_hand, dealer_hand, user_score, dealer_score)
             print("You went over! You lose!")
+        # display hand, dealer 1st card, prompt to hit or stay
         else:
             print(f"Your cards: {user_hand}, current score: {user_score}")
             print(f"Dealer's first card: {dealer_hand[0]}")
             hit = (input("Type 'y' to get another card, type 'n' to pass: "))
+            # if hit, draw another card and append to hand
             if hit == 'y':
                 user_hand.append(deal_card())
-                # Test print
-                print(f"user_hand: {user_hand}, score: {user_score}")
-                 # Test print
-                print(f"dealer_hand: {dealer_hand}, score: {dealer_score}")
+                user_score = calculate_score(user_hand)
+            # If dealer under 17, draw card and append to hand
             if dealer_score < 17:
                 dealer_hand.append(deal_card())
+                dealer_score = calculate_score(dealer_hand)
+            # Pass or stay
             elif hit == 'n':
                 playing = False
-                print(f"Your final hand: {user_hand}, final score: {user_score}")
-                print(f"Dealer's final hand: {dealer_hand}, final score: {dealer_score}")
+                dealer_score = calculate_score(dealer_hand)
+                user_score = calculate_score(user_hand)
+                final_score(user_hand, dealer_hand, user_score, dealer_score)
                 if user_score == dealer_score:
                     print("It's a draw!")
                 elif dealer_score > 21:
@@ -85,14 +94,22 @@ def main():
                     print("You win!")
                 else:
                     print("Dealer wins, you lose!")
-            
+    # Ask player if they want to play again
+    again = input("Would you like to play again? Type 'y' or 'n': ")
+    if again == 'y':
+        clear()
+        start()
+    else:
+        print("Thanks for playing!")
    
 
 
+def start():
+    start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+    if start == 'y':
+        main()
+    else:
+        print("Thanks for playing. Goodbye.")
 
-start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
-if start == 'y':
-    main()
-else:
-    print("Thanks for playing. Goodbye.")
+start()
         
